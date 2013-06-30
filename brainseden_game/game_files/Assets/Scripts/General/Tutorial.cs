5034b4401cec4f98dc376a59cc84253c4f3bfd22
Fries Boury <fries.boury@hotmail.com>
2013-06-30 16:03:15 +0100

    tutorials testing

--- brainseden_game/game_files/Assets/Scripts/General/Tutorial.cs
+++ brainseden_game/game_files/Assets/Scripts/General/Tutorial.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class Tutorial : MonoBehaviour {
 	
@@ -13,12 +14,16 @@ public class Tutorial : MonoBehaviour {
 	private float _waitTimer;
 	// Use this for initialization
 	void Start () {
-	
+		Player.GetComponent<PlayerController>().AnchorGrabbed += AnchorGrabbed;
+		_waitTimer = WaitInterval;
 	}
+
+
 	
 	public void Play()
 	{
 		Enabled = true;
+		
 	}
 	
 	void AnchorGrabbed(object sender, EventArgs e)
@@ -32,22 +37,26 @@ public class Tutorial : MonoBehaviour {
 	{
 		if(!Enabled)return;
 		
-		if(Player.GetComponent<PlayerController>().IsGrabbingAnchor)
-		{
 			if(_waitTimer > 0)
 			{
 				_waitTimer -= Time.deltaTime;
 				if(_waitTimer <=0)
 				{
 					//Jump to next part
-					
+					Vector3 nextPos = Anchors[currentAnchorIndex].position;
+					Vector3 toNextPos = nextPos - transform.position;
+					toNextPos*= 5f;
+					Debug.Log(String.Format("Launching Player: {0}", toNextPos));
+					Player.GetComponent<PlayerController>().LaunchCharacter(Player.transform.position,Player.transform.position + toNextPos);
 					
 					++currentAnchorIndex;
 					if(currentAnchorIndex >= Anchors.Count) Enabled = false;
 					
 				}
 			}
-		}
+		
+		
+		
 		
 		
 	}
